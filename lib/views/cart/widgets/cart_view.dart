7cbcccc2a82a14d39f87350d8aa5208e3fe0cfcd
mahmood <be.ninetythree@gmail.com>
2023-08-12 23:58:53 +0600

    Added Custome Cart Appbar

--- lib/views/cart/widgets/cart_view.dart
+++ lib/views/cart/widgets/cart_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_app/animation/fade_animation.dart';
+import 'package:shoe_app/data/dummy_data.dart';
+import 'package:shoe_app/theme/custom_app_theme.dart';
 import 'package:shoe_app/utils/constants.dart';
 import 'package:shoe_app/views/cart/widgets/empty_state.dart';
 
@@ -10,16 +13,60 @@ class CartView extends StatefulWidget {
 }
 
 class _CartViewState extends State<CartView> {
+  int lengthOfCartItems = itemsOnBag.length;
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: AppConstantsColor.backgroundColor,
-      body: SizedBox(
-        width: width,
-        height: height,
-        child: const EmptyState(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppConstantsColor.backgroundColor,
+        body: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            width: width,
+            height: height,
+            child: Column(
+              children: [
+                _cartAppBar(width: width, height: height, lengthOfCartItems: lengthOfCartItems)
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class _cartAppBar extends StatelessWidget {
+  const _cartAppBar({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.lengthOfCartItems,
+  });
+
+  final double width;
+  final double height;
+  final int lengthOfCartItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height / 14,
+      child: FadeAnimation(
+        delay: 0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "My Cart",
+              style: AppThemes.bagTitle,
+            ),
+            Text(
+              "Total $lengthOfCartItems Items",
+              style: AppThemes.bagTotalPrice,
+            )
+          ],
+        ),
       ),
     );
   }

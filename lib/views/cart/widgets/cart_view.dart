2008c1b85ef740b631d743dd0be25c592e3ab647
mahmood <be.ninetythree@gmail.com>
2023-08-12 23:47:03 +0600

    Attached  empty state widget in cart view screen

--- lib/views/cart/widgets/cart_view.dart
+++ lib/views/cart/widgets/cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shoe_app/utils/constants.dart';
+import 'package:shoe_app/views/cart/widgets/empty_state.dart';
 
 class CartView extends StatefulWidget {
   const CartView({super.key});
@@ -11,8 +12,15 @@ class CartView extends StatefulWidget {
 class _CartViewState extends State<CartView> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppConstantsColor.backgroundColor,
+      body: SizedBox(
+        width: width,
+        height: height,
+        child: const EmptyState(),
+      ),
     );
   }
 }

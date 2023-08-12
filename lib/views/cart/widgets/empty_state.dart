7dc031ccca0a79c9530d972ad17bf2fb4140d38c
mahmood <be.ninetythree@gmail.com>
2023-08-12 23:41:56 +0600

    Added empty state for cart view

---
+++ lib/views/cart/widgets/empty_state.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shoe_app/animation/fade_animation.dart';
+import 'package:shoe_app/theme/custom_app_theme.dart';
+
+class EmptyState extends StatelessWidget {
+  const EmptyState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: width,
+        height: height / 1.4,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeAnimation(
+              delay: 0.5,
+              child:
+                  Text("No shoes added!", style: AppThemes.bagEmptyListTitle),
+            ),
+            FadeAnimation(
+              delay: 0.5,
+              child: Text(
+                "Once you have added, come back:)",
+                style: AppThemes.bagEmptyListSubTitle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
